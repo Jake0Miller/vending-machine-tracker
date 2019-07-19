74b9fd565d4171e571a37d6130b97266af654bfe
Jake Miller <jake0miller@gmail.com>
2019-07-19 09:46:01 -0600

    Finish user story 3

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,9 +1,14 @@
 class Machine < ApplicationRecord
   validates_presence_of :location
   belongs_to :owner
-  has_many :snacks
+  has_many :machine_snacks
+  has_many :snacks, through: :machine_snacks
 
   def average_snack_price
     snacks.average(:price)
+  end
+
+  def item_count
+    snacks.length
   end
 end
---
+++ spec/features/user_can_see_snack_show_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'When a user visits a snack show page', type: :feature do
+  scenario 'they see the location of that machine' do
+    owner = Owner.create!(name: "Sam's Snacks")
+    dons  = owner.machines.create!(location: "Don's Mixed Drinks")
+    rons  = owner.machines.create!(location: "Ron's Mixed Drinks")
+    jons  = owner.machines.create!(location: "Jon's Mixed Drinks")
+    wcb = dons.snacks.create!(name: 'White Castle Burger', price: 3.50)
+    rocks = dons.snacks.create!(name: 'Pop Rocks', price: 1.50)
+    cheetos = dons.snacks.create!(name: 'Flamin Cheetos', price: 2.50)
+    rons.snacks << wcb
+    doritos = rons.snacks.create!(name: 'Flamin Doritos', price: 2.45)
+    jerky = rons.snacks.create!(name: 'Flamin Jerky', price: 2.85)
+    jerky = jons.snacks.create!(name: 'Regular Jerky', price: 1.03)
+
+    visit snack_path(wcb)
+
+    expect(page).to have_content("White Castle Burger")
+    expect(page).to have_content("Price: $3.50")
+    # within "machine-#{dons.id}" do
+      expect(page).to have_content("Location: Don's Mixed Drinks")
+      expect(page).to have_content("Average Price: $2.50")
+      expect(page).to have_content("Item Count: 3")
+    # end
+    expect(page).to have_content("Location: Ron's Mixed Drinks")
+    expect(page).to have_content("Average Price: $2.93")
+    expect(page).to have_content("Item Count: 3")
+  end
+end
