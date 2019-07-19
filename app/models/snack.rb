c468018773661699ce79742cac3e3be35c0e578d
Jake Miller <jake0miller@gmail.com>
2019-07-19 09:13:13 -0600

    First two user stories

---
+++ app/models/snack.rb
@@ -0,0 +1,4 @@
+class Snack < ApplicationRecord
+  validates_presence_of :name, :price
+  belongs_to :machine
+end
--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'When a user visits a vending machine show page', type: :feature do
   scenario 'they see the location of that machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    owner = Owner.create!(name: "Sam's Snacks")
+    dons  = owner.machines.create!(location: "Don's Mixed Drinks")
+    wcb = dons.snacks.create!(name: 'White Castle Burger', price: 3.50)
+    rocks = dons.snacks.create!(name: 'Pop Rocks', price: 1.50)
+    cheetos = dons.snacks.create!(name: 'Flamin Cheetos', price: 2.50)
 
     visit machine_path(dons)
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+    expect(page).to have_content("White Castle Burger")
+    expect(page).to have_content("Price: $3.50")
+    expect(page).to have_content("Price: $1.50")
+    expect(page).to have_content("Price: $2.50")
+    expect(page).to have_content("Average Price: $2.50")
   end
 end
